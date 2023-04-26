9b655063c37b66c631ac46f7b767bc586bab91da
Balan <balan1.618@gmail.com>
2023-04-26 15:55:44 +0530

    Added firebase functions for BE

--- server/functions/app.js
+++ server/functions/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const elastic = require('./elastic');
 
@@ -7,43 +8,24 @@ const DEFAULT_END = '2023-04-01';
 
 const app = express();
 
+app.use(cors({ origin: true }));
+
 app.use((req, res, next) => {
     const from = req.query.from || DEFAULT_START;
     const to = req.query.to || DEFAULT_END;
     const neighbourhood = req.query.neighbourhood || null;
-
     req.parsedParams = { from, to, neighbourhood };
-
     next();
-})
-
-app.get('/api/raw', (request, response) => {
-    const { from, to, neighbourhood } = request.parsedParams;
-    Promise.all([
-        elastic.constructLineGraphData(from, to, neighbourhood),
-        elastic.constructToIssuesData(from, to, neighbourhood),
-        elastic.constructTableData(from, to, neighbourhood),
-        // getRawDataCount(from, to, neighbourhood)
-    ]).then((data) => {
-        const tableData = data[2];
-        response.send({
-            chartData: data[0],
-            issuesData: data[1],
-            rawData: tableData.rows,
-            count: tableData.total,
-        })
-    })
 });
 
-
-app.get('/api/linechart', (request, response) => {
+app.get('/linechart', (request, response) => {
     const { from, to, neighbourhood } = request.parsedParams;
     elastic.constructLineGraphData(from, to, neighbourhood).then((data) => {
         response.send(data);
     });
 });
 
-app.get('/api/table', (request, response) => {
+app.get('/table', (request, response) => {
     const { from, to, neighbourhood } = request.parsedParams;
     const offset = request.query.offset || 0;
     Promise.all([
@@ -55,14 +37,11 @@ app.get('/api/table', (request, response) => {
         })
 });
 
-app.get('/api/topissues', (request, response) => {
+app.get('/topissues', (request, response) => {
     const { from, to, neighbourhood } = request.parsedParams;
     elastic.constructToIssuesData(from, to, neighbourhood).then((data) => {
         response.send(data);
     });
 });
 
-
-
-
-app.listen(3000);
+module.exports = app;
--- server/functions/elastic.js
+++ server/functions/elastic.js
@@ -14,7 +14,6 @@ const client = new Client({
 });
 
 
-
 function fromToAndNeighbourhoodQueryFilter(from, to, neighbourhood) {
     let filters = [
         {
